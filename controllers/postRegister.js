f67389fae4bbf410926e7292b2632830b9c8d6f3
Shashank Dhakate <shashankdkte@gmail.com>
2023-04-30 04:37:11 +0530

    ADDED REGISTER FUNCTIONAITY WIHOUT JWT

--- controllers/postRegister.js
+++ controllers/postRegister.js
@@ -1,4 +1,36 @@
-const postRegister = (req, res) => {
-  res.send("register route");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+
+const postRegister = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    //check if user exist in db
+    const userExists = await User.exists({ email: email.toLowerCase() });
+    if (userExists) {
+      return res.status(409).send("Email already in use");
+    }
+
+    //storing encrypted password in database
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      email: email.toLowerCase(),
+      encryptedPassword,
+    });
+
+    //get jwt token
+    const token = "JWT TOKEN";
+    res.status(201).json({
+      userDetails: {
+        email: user.email,
+        token: token,
+        username: user.username,
+      },
+    });
+  } catch (error) {
+    return res.status(500).send("Error occured. Please try again");
+  }
 };
 module.exports = postRegister;
